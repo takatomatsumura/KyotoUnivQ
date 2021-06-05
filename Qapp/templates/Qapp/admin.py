f880640a92501f7886b09e1079fc484947f3a41d
R_Funa <funaha0318@icloud.com>
2021-06-05 19:42:55 +0900

    templatesフォルダの修正

--- Qapp/templates/Qapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import QuestionModel, AnswerModel, GoodModel, MessageModel
-
-# Register your models here.
-
-admin.site.register(MessageModel)
-admin.site.register(GoodModel)
-admin.site.register(QuestionModel)
-admin.site.register(AnswerModel)
